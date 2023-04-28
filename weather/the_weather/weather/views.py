63df0128c97e2d8f47a7a0159d790f7f04838a50
fb0801 <fbhatti0801@gmail.com>
2023-04-28 19:30:56 +0100

    adding apis variables

--- weather/the_weather/weather/views.py
+++ weather/the_weather/weather/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 def index(request):
+    url = "https://api.openweathermap.org/data/2.5/weather?={}&units=metric&appid="
+    city = "London"
+
+    r = requests.get(url.format(city)).json()
+
+    city_weather = {
+    'city': ,
+    'temperature': '',
+    'description': '',
+    'icon':,
+
+    }
+
     return render(request, 'weather/weather.html')
